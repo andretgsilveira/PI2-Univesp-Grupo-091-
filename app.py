a75109963de2fdc484c9ac877244004e9e91c427
andretgsilveira <andre_tgsilveira@hotmail.com>
2022-11-19 21:20:36 -0300

    Efetuado modificações para exposição de api

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 cloudinary.config(cloud_name = os.getenv('CLOUD_NAME'), api_key=os.getenv('API_KEY'), 
     api_secret=os.getenv('API_SECRET'))
 
-ENV = 'prod'
+ENV = 'env'
 print( os.environ.get('DATABASE_URL', ''))
 
 if ENV == 'dev':
